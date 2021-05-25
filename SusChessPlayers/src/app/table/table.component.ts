769af2dab2f2b057994120f4bd9063bd081db2a5
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2021-05-24 23:56:20 -0400

    Basic table displaying data. Definitely needs to be reformatted and more reusable for filtering

--- SusChessPlayers/src/app/table/table.component.ts
+++ SusChessPlayers/src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  @Input() data;
 
+  tableRows;
+  tableCols;
   constructor() { }
 
   ngOnInit(): void {
+    this.tableRows = Object.keys(this.data);
+    this.tableCols = Object.keys(this.data[this.tableRows[0]]); // terrible
+    console.log(this.tableRows);
+    console.log(this.tableCols);
+    console.log(this.data);
   }
 
 }
