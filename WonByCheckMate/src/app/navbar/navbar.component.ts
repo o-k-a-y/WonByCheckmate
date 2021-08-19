e9df3623e77c22f512622490fb16ff10cc9e0df2
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2021-08-19 00:17:54 -0400

    Removed unnecessary styling to navbar elements, changed text size of input field for user-form to be readable on most screen sizes

--- WonByCheckMate/src/app/navbar/navbar.component.ts
+++ WonByCheckMate/src/app/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { Component, HostListener, SimpleChange, SimpleChanges, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav/sidenav';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -13,10 +13,12 @@ export class NavbarComponent {
   @ViewChild('drawer') drawer!: MatSidenav;
 
   // When the screen hits this size, the breakpoint observer is triggered
-  maxWidth: string = '(max-width: 650px)';
-  minWidth: string = '(min-width: 650px)';
+  // width: SimpleChange = new SimpleChange(undefined, undefined, true);
+  maxWidth: number = 650;
+  maxWidthPx: string = `(max-width: ${this.maxWidth}px)`;
+  minWidthPx: string = `(min-width: ${this.maxWidth}px)`;
 
-  smallScreen$: Observable<boolean> = this.breakpointObserver.observe(this.maxWidth)
+  smallScreen$: Observable<boolean> = this.breakpointObserver.observe(this.maxWidthPx)
     .pipe(
       map(result => result.matches),
       shareReplay()
@@ -24,10 +26,20 @@ export class NavbarComponent {
 
   constructor(private breakpointObserver: BreakpointObserver) { }
 
+  // Close the side navbar if the screen width becomes larger than width
+  // A better solution may be possible because this can still attempt to "close" the side navbar even if it's not open
+  // Closing it means to set MatDrawerToggleResult to 'close'
   @HostListener('window:resize', ['$event'])
-  onResize(event) {
-    if (event.target.innerWidth > 650) {
+  closeSideNavbar(event) {
+    // This solution may actually be slower than leaving it alone
+    // this.width.previousValue = this.width.currentValue;
+    // this.width.currentValue = event.target.innerWidth;
+    // if (this.width.currentValue > this.maxWidth && this.width.previousValue !== undefined && this.width.previousValue < this.maxWidth) {
 
+    //   this.drawer.close();
+    // }
+
+    if (event.target.innerWidth > this.maxWidth) {
       this.drawer.close();
     }
   }
