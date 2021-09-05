dd3bbb1464f3026540168f7c449aadc8eebd7138
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2021-09-05 01:35:41 -0400

    Added loading icon with a light and dark version

--- WonByCheckMate/src/app/loader/loader.component.ts
+++ WonByCheckMate/src/app/loader/loader.component.ts
@@ -7,10 +7,16 @@ import { LoaderService } from './loader.service';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
+  isDarkTheme: boolean = false;
+
+  dark: string = 'dark';
+  light: string = 'light';
+  localStorageTheme: string = 'theme';
 
   constructor(public loaderService: LoaderService) { }
 
   ngOnInit(): void {
+    this.isDarkTheme = localStorage.getItem(this.localStorageTheme) === this.dark ? true : false;
   }
 
 }
