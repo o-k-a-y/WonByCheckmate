4477b70f06d74a0276f22679c3dc44e6952915e5
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2021-09-05 01:53:54 -0400

    Fixed loading screen for light/dark theme

--- WonByCheckMate/src/app/loader/loader.component.ts
+++ WonByCheckMate/src/app/loader/loader.component.ts
@@ -7,8 +7,6 @@ import { LoaderService } from './loader.service';
   styleUrls: ['./loader.component.scss']
 })
 export class LoaderComponent implements OnInit {
-  isDarkTheme: boolean = false;
-
   dark: string = 'dark';
   light: string = 'light';
   localStorageTheme: string = 'theme';
@@ -16,7 +14,11 @@ export class LoaderComponent implements OnInit {
   constructor(public loaderService: LoaderService) { }
 
   ngOnInit(): void {
-    this.isDarkTheme = localStorage.getItem(this.localStorageTheme) === this.dark ? true : false;
+  }
+
+
+  isDarkTheme(): boolean {
+    return localStorage.getItem(this.localStorageTheme) === this.dark ? true : false;
   }
 
 }
