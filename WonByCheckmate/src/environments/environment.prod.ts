62d77c5eb77b1cebf1aa0f77a5a6cb8bd4c6bc50
o-k-a-y <22223097+o-k-a-y@users.noreply.github.com>
2021-09-12 23:00:11 -0400

    Updated baseUrl

--- WonByCheckmate/src/environments/environment.prod.ts
+++ WonByCheckmate/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "https://wonbycheckmate.com/api" // might need to add www. for it to work
+  baseUrl: "https://wonbycheckmate.azurewebsites.net/api" // might need to add www. for it to work
 };
